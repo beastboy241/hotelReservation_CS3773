8d7ef5aeef450ba98f56f709da956eba7e65b040
SomeGuyNamedJon <60898789+SomeGuyNamedJon@users.noreply.github.com>
2021-10-29 01:59:27 -0500

    Bad practice fix/ code style update
    Removed an unnecessary map from SingleHotel.js and saved it and Hotel.js with prettier extension on vs code to make code less of an eye strain to read

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,43 +1,50 @@
 import Hero from "../components/Hero";
 import Axios from "axios";
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 const Hotels = () => {
-  
   const [hotelList, setHotelList] = useState([]);
-  
-  useEffect(()=>{
-    Axios.get('http://localhost:3001/api/get/hotels').then((response)=> {
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get/hotels").then((response) => {
       setHotelList(response.data);
     });
   }, []);
 
-  {/*Amenities are a single int value decoded bitwise
+  {
+    /*Amenities are a single int value decoded bitwise
     1000 or 8 is the Pool
     0100 or 4 is the Gym
     0010 or 2 is the Spa
     0001 or 1 is the Business Office
           
-    You can get whatever amenity you are looking for by using bitwise AND*/}
-  
+    You can get whatever amenity you are looking for by using bitwise AND*/
+  }
+
   return (
     <>
-    {hotelList.map((val)=>{
-      return <a href={"http://localhost:3000/hotels/" + val.id} style={{textDecoration: 'none'}} key={val.id}>
-        <div className="hotels">
-        <h2 style={{float: 'left'}}>{val.name}</h2>
-        <h5 style={{float: 'right'}}>
-          {(val.amenities & 8) ? '🏊 ' : ''}
-          {(val.amenities & 4) ? '💪 ' : ''}
-          {(val.amenities & 2) ? '🧴 ' : ''}
-          {(val.amenities & 1) ? '💼 ' : ''}
-        </h5>
-        <br /><br />
-        </div></a>
-       })}
+      {hotelList.map((val) => {
+        return (
+          <a
+            href={"http://localhost:3000/hotels/" + val.id}
+            style={{ textDecoration: "none" }}
+            key={val.id}
+          >
+            <div className="hotels">
+              <h2 style={{ float: "left" }}>{val.name}</h2>
+              <h5 style={{ float: "right" }}>
+                {val.amenities & 8 ? "🏊 " : ""}
+                {val.amenities & 4 ? "💪 " : ""}
+                {val.amenities & 2 ? "🧴 " : ""}
+                {val.amenities & 1 ? "💼 " : ""}
+              </h5>
+              <br />
+              <br />
+            </div>
+          </a>
+        );
+      })}
     </>
-
   );
 };
 export default Hotels;
--- src/pages/SingleHotel.js
+++ src/pages/SingleHotel.js
@@ -1,49 +1,78 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import Hero from "../components/Hero";
 
 const SingleHotel = () => {
-    const [hotel, setHotel] = useState([]);
-    let id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+  const [hotel, setHotel] = useState([]);
+  let id = window.location.href.substring(
+    window.location.href.lastIndexOf("/") + 1
+  );
 
-    useEffect(()=>{
-        Axios.post('http://localhost:3001/api/get/hotel', {hotelId: id}).then((response)=> {
-          setHotel(response.data);
-        });
-    }, []);
+  useEffect(() => {
+    Axios.post("http://localhost:3001/api/get/hotel", { hotelId: id }).then(
+      (response) => {
+        setHotel(response.data[0]);
+      }
+    );
+  }, []);
 
   return (
-    <>
-    {hotel.map((val)=>{
-      return <div className="hotels">
-        <h2 style={{fontWeight: 'bold', float: 'left'}}>{val.name}</h2>
-
-        <div style={{float: 'right'}}>
-          {(val.amenities & 8) ? '🏊 ' : ''}
-          {(val.amenities & 4) ? '💪 ' : ''}
-          {(val.amenities & 2) ? '🧴 ' : ''}
-          {(val.amenities & 1) ? '💼 ' : ''}
-        </div>
-        <br /> <br />
-        <h3 style={{textAlign: 'center'}}>{val.rooms} rooms</h3>
-
-        <div style={{float: 'left'}}>
-        <h4 style={{textIndent: 20}}>Weekday</h4>
-        <h5 style={{textIndent: 40}}>{val.standard_price ? "Standard: $" + val.standard_price.toFixed(2) : ""}</h5>
-        <h5 style={{textIndent: 40}}>{val.queen_price ? "Queen: $" + val.queen_price.toFixed(2) : ""}</h5>
-        <h5 style={{textIndent: 40}}>{val.king_price ? "King: $" + val.king_price.toFixed(2) : ""}</h5>
-        </div>
-
-        <div>
-        <h4 style={{textIndent: 20}}>Weekend</h4>
-        <h5 style={{textIndent: 40}}>{val.standard_price ? "Standard: $" + (val.standard_price + val.standard_price*val.weekend_differential).toFixed(2) : ""}</h5>
-        <h5 style={{textIndent: 40}}>{val.queen_price ? "Queen: $" + (val.queen_price + val.queen_price*val.weekend_differential).toFixed(2) : ""}</h5>
-        <h5 style={{textIndent: 40}}>{val.king_price ? "King: $" + (val.king_price + val.king_price*val.weekend_differential).toFixed(2) : ""}</h5>
-        </div>
+    <div className="hotels">
+      <h2 style={{ fontWeight: "bold", float: "left" }}>{hotel.name}</h2>
+      <div style={{ float: "right" }}>
+        {hotel.amenities & 8 ? "🏊 " : ""}
+        {hotel.amenities & 4 ? "💪 " : ""}
+        {hotel.amenities & 2 ? "🧴 " : ""}
+        {hotel.amenities & 1 ? "💼 " : ""}
       </div>
-    })}
-    </>
-  );  
+      <br /> <br />
+      <h3 style={{ textAlign: "center" }}>{hotel.rooms} rooms</h3>
+      <div style={{ float: "left" }}>
+        <h4 style={{ textIndent: 20 }}>Weekday</h4>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.standard_price
+            ? "Standard: $" + hotel.standard_price.toFixed(2)
+            : ""}
+        </h5>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.queen_price ? "Queen: $" + hotel.queen_price.toFixed(2) : ""}
+        </h5>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.king_price ? "King: $" + hotel.king_price.toFixed(2) : ""}
+        </h5>
+      </div>
+      <div>
+        <h4 style={{ textIndent: 20 }}>Weekend</h4>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.standard_price
+            ? "Standard: $" +
+              (
+                hotel.standard_price +
+                hotel.standard_price * hotel.weekend_differential
+              ).toFixed(2)
+            : ""}
+        </h5>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.queen_price
+            ? "Queen: $" +
+              (
+                hotel.queen_price +
+                hotel.queen_price * hotel.weekend_differential
+              ).toFixed(2)
+            : ""}
+        </h5>
+        <h5 style={{ textIndent: 40 }}>
+          {hotel.king_price
+            ? "King: $" +
+              (
+                hotel.king_price +
+                hotel.king_price * hotel.weekend_differential
+              ).toFixed(2)
+            : ""}
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default SingleHotel;
