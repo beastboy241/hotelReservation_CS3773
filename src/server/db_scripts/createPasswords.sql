448b0b913b057dc7af856dc7ae5b5fbe9bd8adde
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2021-11-06 13:43:56 -0500

    Removed unused sql files
    More merge residue

--- src/server/db_scripts/createPasswords.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists passwords;
-create table `passwords` (
-    `usr_id` int not null,
-    `password` varchar(200) not null,
-    primary key (`usr_id`)
-);
--- src/server/db_scripts/createUsers.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists users;
-create table `users` (
-    `id` int not null auto_increment,
-    `firstName` varchar(100),
-    `lastName` varchar(100),
-    `email` varchar(100),
-    `type` char not null,
-    primary key (`id`)
-);
