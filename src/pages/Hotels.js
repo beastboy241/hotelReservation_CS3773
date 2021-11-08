2a2a7c082b410715ac03eb982f61407f02be8846
Loc Nguyen <31332651+beastboy241@users.noreply.github.com>
2021-11-08 11:27:35 -0600

    update filter funct

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,43 +1,97 @@
-import Hero from "../components/Hero";
 import Axios from "axios";
 import React, { useState, useEffect } from "react";
-import _searchBar from '../components/searchBar';
-import '../css/styles.css';
-
-
+import _searchBar from "../components/searchBar";
+import "../css/styles.css";
+import {Amenity} from "../components/AmenityTable";
 
 
 
 const Hotels = () => {
- 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [hotelListDefault, setHotelListDefault] = useState();
   const [hotelList, setHotelList] = useState([]);
- 
+
+  const [filter, setFilter] = useState();
 
   const fetchData = async () => {
-    return await Axios.get("http://localhost:3001/api/get/hotels").then((response) => {
-      setHotelListDefault(response.data);
-      setHotelList(response.data);
+    return await Axios.get("http://localhost:3001/api/get/hotels").then(
+      (response) => {
+        setHotelListDefault(response.data);
+        setHotelList(response.data);
+        setFilter(response.data);
+
+      }
+    );
+
+  };
+
+  const updateInput = async () => {
+    setInput(document.getElementById("input").value);
+    console.log("Got Here");
+    const filtered = hotelListDefault.filter((hotel) => {
+      return hotel.name.toLowerCase().includes(input.toLowerCase());
     });
+    //setInput(input);
+    //setHotelList(filtered);
 
-  }
-
-  const updateInput = async (input) => {
-    const filtered = hotelListDefault.filter(hotel => {
-      return hotel.name.toLowerCase().includes(input.toLowerCase())
-    })
-      setInput(input);
+    if (input === "") {
+      setHotelList([]);
+    } else {
       setHotelList(filtered);
+    }
+  };
+
+// Pool chekced
+const updatePool= async () => {
+  setFilter(document.getElementById("input").value);
+  console.log("Got Here");
+  const filtered = hotelListDefault.filter((hotel) => {
+
+    return hotel.amenities & Amenity.POOL;
+  });
+    setHotelList(filtered);
+  
+}
+  
+// Gym checked
+  const updateGym = async () => {
+    setFilter(document.getElementById("input").value);
+    console.log("Got Here");
+    const filtered = hotelListDefault.filter((hotel) => {
+
+      return hotel.amenities & Amenity.GYM;
+    });
+      setHotelList(filtered);
+    
   }
 
-  /* useEffect(() => {
-    Axios.get("http://localhost:3001/api/get/hotels").then((response) => {
-      setHotelList(response.data);
-    });
-  }, []); */
-  useEffect( () => {fetchData()}, []);
+// Spa checked
+const updateSpa= async () => {
+  setFilter(document.getElementById("input").value);
+  console.log("Got Here");
+  const filtered = hotelListDefault.filter((hotel) => {
+
+    return hotel.amenities & Amenity.SPA;
+  });
+    setHotelList(filtered);
   
+}
+// Office checked
+const updateOffice= async () => {
+  setFilter(document.getElementById("input").value);
+  console.log("Got Here");
+  const filtered = hotelListDefault.filter((hotel) => {
+
+    return hotel.amenities & Amenity.OFFICE;
+  });
+    setHotelList(filtered);
+  
+}
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   {
     /*Amenities are a single int value decoded bitwise
     1000 or 8 is the Pool
@@ -48,35 +102,51 @@ const Hotels = () => {
     You can get whatever amenity you are looking for by using bitwise AND*/
   }
 
+  
+
   return (
     <>
-      <h1 className="hotelTitle">Hotel List  </h1> 
-      <_searchBar className="searchbar" input = {input}
-        onChange={updateInput} />
+
+      <div className="search_section">
+        <input className="searchBar" type="search" placeholder={"search hotel..."} id="input" onChange={updateInput} />
+      
+
+        <div className="checkbox">
+          <table>
+            <tr>
+          <td> 
+            <label><input type="checkbox" rel="pool" onClick={updatePool}/>Pool</label></td>
+          <td> 
+          <label><input type="checkbox" rel="gym" onClick={updateGym}/>Gym</label></td> 
+          <td> 
+          <label><input type="checkbox" rel="spa" onClick={updateSpa}/>Spa</label></td> 
+          <td> 
+          <label><input type="checkbox" rel="office" onClick={updateOffice}/>Bussiness Office</label></td> 
+            </tr>
+          </table>
+        </div>
+      </div>
+      
+     
+      
+
       {hotelList.map((val) => {
-        
         return (
-          
           <a
             href={"http://localhost:3000/hotels/" + val.id}
             style={{ textDecoration: "none" }}
             key={val.id}
           >
-            
-            <div className="hotels">
-              
-              <h2 style={{ float: "left" }}>{val.name}</h2>
-              <h5 style={{ float: "right" }}>
-                {val.amenities & 8 ? "🏊 " : ""}
-                {val.amenities & 4 ? "💪 " : ""}
-                {val.amenities & 2 ? "🧴 " : ""}
-                {val.amenities & 1 ? "💼 " : ""}
-              </h5>
-              <br />
-              <br />
-            </div>
+              <div className="hotels">
+                      <h2>{val.name}</h2>
+                        <h5>
+                          {val.amenities & Amenity.POOL ? <i className="fas fa-water"> Pool </i>: ""}
+                          {val.amenities & Amenity.GYM ? <i className="fas fa-dumbbell"> Gym </i> : ""}
+                          {val.amenities & Amenity.SPA ? <i className="fas fa-spa"> Spa </i> : ""}
+                          {val.amenities & Amenity.OFFICE ? <i className="fas fa-briefcase"> Business Office </i> : ""}
+                        </h5>
+              </div>
           </a>
-          
         );
       })}
     </>
