c45ee6bb40d80125e948449ed12002e953232c32
SomeGuyNamedJon <60898789+SomeGuyNamedJon@users.noreply.github.com>
2021-11-12 09:30:40 -0600

    Merge branch 'main' into main(mine)

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import React, { useState, useEffect } from "react";
 import _searchBar from "../components/searchBar";
 import "../css/styles.css";
-import {Amenity} from "../components/AmenityTable";
+import { Amenity } from "../components/AmenityTable";
 
 const Hotels = () => {
   const [input, setInput] = useState("");
@@ -17,10 +17,8 @@ const Hotels = () => {
         setHotelListDefault(response.data);
         setHotelList(response.data);
         setFilter(response.data);
-
       }
     );
-
   };
 
   const updateInput = async () => {
@@ -29,85 +27,95 @@ const Hotels = () => {
     const filtered = hotelListDefault.filter((hotel) => {
       return hotel.name.toLowerCase().includes(input.toLowerCase());
     });
-    
+
     setHotelList(filtered);
   };
 
-// Pool chekced
-const updatePool= async () => {
-  setFilter(document.getElementById("input").value);
-  console.log("Got Here");
-  const filtered = hotelListDefault.filter((hotel) => {
-
-    return hotel.amenities & Amenity.POOL;
-  });
+  // Pool chekced
+  const updatePool = async () => {
+    setFilter(document.getElementById("input").value);
+    console.log("Got Here");
+    const filtered = hotelListDefault.filter((hotel) => {
+      return hotel.amenities & Amenity.POOL;
+    });
     setHotelList(filtered);
-  
-}
-  
-// Gym checked
+  };
+
+  // Gym checked
   const updateGym = async () => {
     setFilter(document.getElementById("input").value);
     console.log("Got Here");
     const filtered = hotelListDefault.filter((hotel) => {
-
       return hotel.amenities & Amenity.GYM;
     });
-      setHotelList(filtered);
-    
-  }
-
-// Spa checked
-const updateSpa= async () => {
-  setFilter(document.getElementById("input").value);
-  console.log("Got Here");
-  const filtered = hotelListDefault.filter((hotel) => {
-
-    return hotel.amenities & Amenity.SPA;
-  });
     setHotelList(filtered);
-  
-}
-// Office checked
-const updateOffice= async () => {
-  setFilter(document.getElementById("input").value);
-  console.log("Got Here");
-  const filtered = hotelListDefault.filter((hotel) => {
+  };
 
-    return hotel.amenities & Amenity.OFFICE;
-  });
+  // Spa checked
+  const updateSpa = async () => {
+    setFilter(document.getElementById("input").value);
+    console.log("Got Here");
+    const filtered = hotelListDefault.filter((hotel) => {
+      return hotel.amenities & Amenity.SPA;
+    });
     setHotelList(filtered);
-  
-}
+  };
+  // Office checked
+  const updateOffice = async () => {
+    setFilter(document.getElementById("input").value);
+    console.log("Got Here");
+    const filtered = hotelListDefault.filter((hotel) => {
+      return hotel.amenities & Amenity.OFFICE;
+    });
+    setHotelList(filtered);
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  
   return (
     <>
-
       <div className="search_section">
-        <input className="searchBar" type="search" placeholder={"search hotel..."} id="input" onChange={updateInput} />
-      
+        <input
+          className="searchBar"
+          type="search"
+          placeholder={"search hotel..."}
+          id="input"
+          onChange={updateInput}
+        />
 
         <div className="checkbox">
           <table>
             <tr>
-          <td> 
-            <label><input type="checkbox" rel="pool" onClick={updatePool}/>Pool</label></td>
-          <td> 
-          <label><input type="checkbox" rel="gym" onClick={updateGym}/>Gym</label></td> 
-          <td> 
-          <label><input type="checkbox" rel="spa" onClick={updateSpa}/>Spa</label></td> 
-          <td> 
-          <label><input type="checkbox" rel="office" onClick={updateOffice}/>Bussiness Office</label></td> 
+              <td>
+                <label>
+                  <input type="checkbox" rel="pool" onClick={updatePool} />
+                  Pool
+                </label>
+              </td>
+              <td>
+                <label>
+                  <input type="checkbox" rel="gym" onClick={updateGym} />
+                  Gym
+                </label>
+              </td>
+              <td>
+                <label>
+                  <input type="checkbox" rel="spa" onClick={updateSpa} />
+                  Spa
+                </label>
+              </td>
+              <td>
+                <label>
+                  <input type="checkbox" rel="office" onClick={updateOffice} />
+                  Bussiness Office
+                </label>
+              </td>
             </tr>
           </table>
         </div>
       </div>
-      
 
       {hotelList.map((hotel) => {
         return (
@@ -116,18 +124,32 @@ const updateOffice= async () => {
             style={{ textDecoration: "none" }}
             key={hotel.id}
           >
-              <div className="hotels">
-                      <h2>{hotel.name}</h2>
-                      <img>{hotel.hotel_image}</img>
-                        <h5>
-                          {hotel.amenities & Amenity.POOL ? <i className="fas fa-water"> Pool </i>: ""}
-                          {hotel.amenities & Amenity.GYM ? <i className="fas fa-dumbbell"> Gym </i> : ""}
-                          {hotel.amenities & Amenity.SPA ? <i className="fas fa-spa"> Spa </i> : ""}
-                          {hotel.amenities & Amenity.OFFICE ? <i className="fas fa-briefcase"> Business Office </i> : ""}
-                        </h5>
-                        
-                        
-              </div>
+            <div className="hotels">
+              <h2>{hotel.name}</h2>
+              <img>{hotel.hotel_image}</img>
+              <h5>
+                {hotel.amenities & Amenity.POOL ? (
+                  <i className="fas fa-water"> Pool </i>
+                ) : (
+                  ""
+                )}
+                {hotel.amenities & Amenity.GYM ? (
+                  <i className="fas fa-dumbbell"> Gym </i>
+                ) : (
+                  ""
+                )}
+                {hotel.amenities & Amenity.SPA ? (
+                  <i className="fas fa-spa"> Spa </i>
+                ) : (
+                  ""
+                )}
+                {hotel.amenities & Amenity.OFFICE ? (
+                  <i className="fas fa-briefcase"> Business Office </i>
+                ) : (
+                  ""
+                )}
+              </h5>
+            </div>
           </a>
         );
       })}
