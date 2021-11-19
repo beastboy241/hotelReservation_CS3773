04462ba906966baf7a2ca362558206ebc9bbb27b
SomeGuyNamedJon <60898789+SomeGuyNamedJon@users.noreply.github.com>
2021-11-19 10:13:32 -0600

    Added admin navbar elements
    Links lead to home right now and css needs to be adjusted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { MenuItems } from "../pages/MenuItems";
-import  '../css/Navbar.css';
-import session from './SessionManager';
-
+import "../css/Navbar.css";
+import session from "./SessionManager";
 
 const Navbar = () => {
-  const [state, setState] = useState ({ clicked: false });
+  const [state, setState] = useState({ clicked: false });
   const user = session.GetUser();
 
   const handleClick = () => {
-    setState({ clicked: !state.clicked })
-  }
+    setState({ clicked: !state.clicked });
+  };
 
-  return(
+  return (
     <nav className="NavbarItems">
-          <h2 className="navbar-logo"><i className="fas fa-hotel"> </i> Hotel Booking</h2>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-          </div>
-          <ul className={state.clicked ? 'nav-menu active' : 'nav-menu'}>
-            {MenuItems.map((item, index) => {
-              let returnFlag = false;
+      <h2 className="navbar-logo">
+        <i className="fas fa-hotel"> </i> Hotel Booking
+      </h2>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={state.clicked ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          let returnFlag = false;
 
-              if(item.type === "default"){
-                returnFlag = true;
-              }else{
-                if(item.type === "logged in" && user.login)
-                  returnFlag = true;
-                if(item.type === "logged out" && !user.login)
-                  returnFlag = true;
-              }
-                
-              if(returnFlag){
-                return (
-                    <li key={index}>
-                        <a className={item.cName} href={item.url}>
-                          {item.title}
-                        </a> 
-                    </li>
-                )
-              }
-          
-            })}
+          if (item.type === "default") {
+            returnFlag = true;
+          } else {
+            if (item.type === "admin" && user.creds === "a") returnFlag = true;
+            if (item.type === "logged in" && user.login) returnFlag = true;
+            if (item.type === "logged out" && !user.login) returnFlag = true;
+          }
 
-              
-          </ul>
-            
+          if (returnFlag) {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          }
+        })}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/pages/MenuItems.js
+++ src/pages/MenuItems.js
@@ -1,39 +1,50 @@
 export const MenuItems = [
   {
-    title: 'Home',
-    url: '/',
-    cName: 'nav-links',
-    type: 'default'
-  },
-
-
-  {
-    title: 'Hotels',
-    url: '/hotels',
-    cName: 'nav-links',
-    type: 'default'
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+    type: "default",
   },
 
   {
-    title: 'My Account',
-    url: '/account',
-    cName: 'nav-links',
-    type: 'logged in'
-  },
-
-
-  {
-    title: 'Login',
-    url: '/login',
-    cName: 'nav-links-mobile',
-    type: 'logged out'
+    title: "Hotels",
+    url: "/hotels",
+    cName: "nav-links",
+    type: "default",
   },
 
   {
-    title: 'Logout',
-    url: '/logout',
-    cName: 'nav-links-mobile',
-    type: 'logged in'
+    title: "My Account",
+    url: "/account",
+    cName: "nav-links",
+    type: "logged in",
   },
 
-]
+  {
+    title: "Users",
+    url: "/",
+    cName: "nav-links-admin",
+    type: "admin",
+  },
+
+  {
+    title: "Reservations",
+    url: "/",
+    cName: "nav-links-admin",
+    type: "admin",
+  },
+
+  {
+    title: "Login",
+    url: "/login",
+    cName: "nav-links-mobile",
+    type: "logged out",
+  },
+
+  {
+    title: "Logout",
+    url: "/logout",
+    cName: "nav-links-mobile",
+    type: "logged in",
+  },
+];
